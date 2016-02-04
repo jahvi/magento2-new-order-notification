367fe4992f13fee7273ad8467fe2caa2e0734288
Javier Villanueva <javiervd@gmail.com>
2016-02-04 22:55:19 +0000

    Remove unnecessary app data from frontend

--- Block/Datajs.php
+++ Block/Datajs.php
@@ -3,22 +3,14 @@ namespace Jahvi\NewOrderNotification\Block;
 
 class Datajs extends \Magento\Framework\View\Element\Template
 {
-    const PUSHER_APP_ID     = 'checkout/newordernotification/app_id';
-    const PUSHER_APP_KEY    = 'checkout/newordernotification/app_key';
-    const PUSHER_APP_SECRET = 'checkout/newordernotification/app_secret';
-
-    public function getAppId()
-    {
-        return $this->_scopeConfig->getValue(self::PUSHER_APP_ID);
-    }
+    const PUSHER_APP_KEY = 'checkout/newordernotification/app_key';
 
     public function getAppKey()
     {
-        return $this->_scopeConfig->getValue(self::PUSHER_APP_KEY);
-    }
-
-    public function getAppSecret()
-    {
-        return $this->_scopeConfig->getValue(self::PUSHER_APP_SECRET);
+        return $this->_scopeConfig->getValue(
+            self::PUSHER_APP_KEY,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            null
+        );
     }
 }
