4edbc9b5fc78ccdc15b0695cc68559783fa8df96
Javier Villanueva <javiervd@gmail.com>
2017-11-12 06:48:17 +0000

    Instantiate Pusher class using DI

--- Observer/DisplayNotification.php
+++ Observer/DisplayNotification.php
@@ -6,6 +6,7 @@
  */
 namespace Jahvi\NewOrderNotification\Observer;
 
+use Pusher\PusherFactory;
 use Magento\Catalog\Helper\Image;
 use Magento\Sales\Model\OrderFactory;
 use Magento\Framework\Event\Observer;
@@ -36,23 +37,31 @@ class DisplayNotification implements ObserverInterface
     private $imageHelper;
 
     /**
+     * @var \Pusher\PusherFactory
+     */
+    private $pusherFactory;
+
+    /**
      * Setup initial dependencies
      *
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $globalConfig
-     * @param \Magento\Sales\Model\OrderFactory                  $orderFactory
-     * @param \Magento\Directory\Model\CountryFactory            $countryFactory
-     * @param \Magento\Catalog\Helper\Image                      $imageHelper
+     * @param \Magento\Sales\Model\OrderFactory $orderFactory
+     * @param \Magento\Directory\Model\CountryFactory $countryFactory
+     * @param \Magento\Catalog\Helper\Image $imageHelper
+     * @param \Pusher\PusherFactory $pusherFactory
      */
     public function __construct(
         ScopeConfigInterface $globalConfig,
         OrderFactory $orderFactory,
         CountryFactory $countryFactory,
-        Image $imageHelper
+        Image $imageHelper,
+        PusherFactory $pusherFactory
     ) {
-        $this->globalConfig   = $globalConfig;
-        $this->orderFactory   = $orderFactory;
+        $this->globalConfig = $globalConfig;
+        $this->orderFactory = $orderFactory;
         $this->countryFactory = $countryFactory;
-        $this->imageHelper    = $imageHelper;
+        $this->imageHelper = $imageHelper;
+        $this->pusherFactory = $pusherFactory;
     }
 
     /**
@@ -67,12 +76,17 @@ class DisplayNotification implements ObserverInterface
             return $this;
         }
 
-        $appId     = $this->globalConfig->getValue('checkout/newordernotification/app_id');
-        $appKey    = $this->globalConfig->getValue('checkout/newordernotification/app_key');
+        $appId = $this->globalConfig->getValue('checkout/newordernotification/app_id');
+        $appKey = $this->globalConfig->getValue('checkout/newordernotification/app_key');
         $appSecret = $this->globalConfig->getValue('checkout/newordernotification/app_secret');
         $cluster = $this->globalConfig->getValue('checkout/newordernotification/cluster');
 
-        $pusher = new \Pusher\Pusher($appKey, $appSecret, $appId, ['encrypted' => true, 'cluster' => $cluster]);
+        $pusher = $this->pusherFactory->create([
+            'auth_key' => $appKey,
+            'secret' => $appSecret,
+            'app_id' => $appId,
+            'options' => ['encrypted' => true, 'cluster' => $cluster]
+        ]);
 
         // Get latest order
         $orderId = $observer->getEvent()->getOrderIds()[0];
@@ -80,23 +94,23 @@ class DisplayNotification implements ObserverInterface
         $order = $this->orderFactory->create()->load($orderId);
 
         // Get last product in order data
-        $product      = $order->getAllVisibleItems()[0]->getProduct();
+        $product = $order->getAllVisibleItems()[0]->getProduct();
         $shippingCity = $order->getShippingAddress()->getCity();
         $productImage = $this->imageHelper->init($product, 'product_thumbnail_image');
 
         // Get shipping city and country
         $shippingCountryCode = $order->getShippingAddress()->getCountryId();
-        $shippingCountry     = $this->countryFactory->create()->loadByCode($shippingCountryCode);
+        $shippingCountry = $this->countryFactory->create()->loadByCode($shippingCountryCode);
 
         // Trigger pusher event with collected data
-        $pusher->trigger(
+        $resp = $pusher->trigger(
             'non_channel',
             'new_order',
             [
-                'product_name'     => $product->getName(),
-                'product_image'    => $productImage->getUrl(),
-                'product_url'      => $product->getProductUrl(),
-                'shipping_city'    => $shippingCity,
+                'product_name' => $product->getName(),
+                'product_image' => $productImage->getUrl(),
+                'product_url' => $product->getProductUrl(),
+                'shipping_city' => $shippingCity,
                 'shipping_country' => $shippingCountry->getName(),
             ]
         );
